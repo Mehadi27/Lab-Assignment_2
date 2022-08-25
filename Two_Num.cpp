54cb48370d4a4767f53aa887ead9b4eeb77149f4
MD. Mehadi hasan <112084970+Mehadi27@users.noreply.github.com>
2022-08-25 19:45:30 +0600

    Add files via upload

---
+++ Two_Num.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+
+  cout << "Enter  the two integers: ";
+  cin >> first_number >> second_number;
+
+
+  sum = first_number + second_number;
+
+
+  cout << first_number << " + " <<  second_number << " = " << sum;
+
+  return 0;
+}
